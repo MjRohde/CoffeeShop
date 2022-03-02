035277e7172ffd1f8f3aa2d96f1826be7b121a13
Mattusio <60032522+Mattusio@users.noreply.github.com>
2022-03-02 19:23:49 +0100

    Added hover effect on cards

--- src/components/Carousel/Card.js
+++ src/components/Carousel/Card.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
 
 function Card({ products }) {
+  const [hover, setHover] = useState(false);
+
   //Gets all information available about a specific product
   let { name, brand, image, price, type } = products;
   return (
     <div
       className="card"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
       style={{
-        backgroundImage: `url(${image})`,
-        backgroundSize: "100% 100%",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center center",
+        background: hover
+          ? `linear-gradient(rgba(04,04,04,0.5), rgba(04,04,04,0.5)), url(${image})`
+          : `url(${image})`,
       }}
     >
-      <div className="cardImage"></div>
       <div className="cardText">
         <h1>{type}</h1>
-        <h2>{name}</h2>
-        <h2>Brand: {brand}</h2>
-        <h3>Price: {parseFloat(price).toFixed(2)}$</h3>
+        {hover && (
+          <div className="textHover">
+            <h2>{name}</h2>
+            <h2>Brand: {brand}</h2>
+            <h3>Price: {parseFloat(price).toFixed(2)}$</h3>
+          </div>
+        )}
       </div>
     </div>
   );
